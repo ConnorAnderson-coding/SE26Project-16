83c3243b1b8fe48b89e18ca4eddf13432a9c4d30
ConnorAnderson-coding <beyond-the-reality@sjtu.edu.cn>
2026-07-09 11:24:18 +0800

    使用根目录下的campus进行后端开发

--- campus-activity(为后端服务的前端代码,可修改)/src/components/AuthGuard.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useApp } from '../context/AppContext'
-
-export default function AuthGuard({ children }) {
-  const { currentUser } = useApp()
-  if (!currentUser) return <Navigate to="/" replace />
-  return children
-}
